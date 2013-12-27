535d2a058dfc2782901ddf33c41a2c6684dcda65
huyettcp <huyettcp@gmail.com>
2013-12-27 15:24:28 -0500

    Posters append, API functional

--- app/assets/javascripts/months.js
+++ app/assets/javascripts/months.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   monthlyData();
 
   $('.year').on('click', function(){
-    var year = parseInt($('input[name=radios]:checked').val())
+    var year = this.id
     changeYear(year);
   });
 
@@ -10,12 +10,29 @@ $(document).ready(function(){
 
 
   $('#scatter').on('click', function(){
-    monthlyData()
+    var year = parseInt($('input[name=radios]:checked').val())
+    monthlyData();
   });
 
 
 
 });
+
+function getMoviePoster(top_movie){
+  console.log(top_movie);
+  var url = "http://www.omdbapi.com/";
+  var params = {
+    t: top_movie
+  };
+  $.getJSON(url, params, showMoviePoster);
+};
+
+function showMoviePoster(data) {
+  movie_data = data;
+  console.log(movie_data.Poster);
+  $("#movie_poster").empty().append("<img id= 'poster' src=" + movie_data.Poster + "/>");
+}
+
 
 
 
@@ -25,14 +42,14 @@ function monthlyData(){
     method: 'GET',
     dataType: 'json'
   }).done(function(month){
-    var w = 1000;
-    var h = 500;
+    var w = 800;
+    var h = 400;
     var padding = 40;
      
 
     month_data = [ 
-      [1, month[0].total_month_gross],
-      [2, month[1].total_month_gross],
+      [1, month[0].total_month_gross, "January", month[0].top_movie_title],
+      [2, month[1].total_month_gross, "February", month[1].top_movie_title],
       [3, month[2].total_month_gross],
       [4, month[3].total_month_gross],
       [5, month[4].total_month_gross],
@@ -46,6 +63,7 @@ function monthlyData(){
     ];
 
 
+
     var months = ["", "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     var formatMonth = function(d) {
       return months[d % 12];      
@@ -56,7 +74,9 @@ function monthlyData(){
     var svg = d3.select("#data_area")
                 .append("svg")
                 .attr("width", w)
-                .attr("height", h);
+                .attr("height", h)
+                .attr("class", "ten columns")
+                .attr("preserveAspectRatio", "none");
 
     var xScale = d3.scale.linear()
                         .domain([0, d3.max(month_data, function(d) { return d[0]; })])
@@ -87,7 +107,19 @@ function monthlyData(){
         .attr("cy", function(d) {
           return yScale(d[1]);
         })
-        .attr("r", 6);
+        .attr("r", 6)
+        .on("mouseover", function(e, i) {
+              $( "#month_name" ).text(e[2])
+              $( "#top_movie" ).text(e[3])
+              top_movie = e[3]
+              getMoviePoster(top_movie)
+              d3.select(this)
+              .attr("fill", "orange");
+        })
+        .on("mouseout", function(d) {
+              d3.select(this)
+              .attr("fill", "rgb(0, 0, " + (d * 10) + ")");
+        });
 
     svg.append("g")
         .attr("class", "axis")
@@ -109,8 +141,8 @@ function changeYear(year){
     method: 'GET',
     dataType: 'json'
   }).done(function(month){
-    var w = 1000;
-    var h = 500;
+    var w = 800;
+    var h = 400;
     var padding = 40;
      
 
@@ -129,9 +161,9 @@ function changeYear(year){
       [12, month[11].total_month_gross]
     ];
 
-
+  
     var svg = d3.select("#data_area")
-               
+             
                 .attr("width", w)
                 .attr("height", h);
     var xScale = d3.scale.linear()
@@ -157,9 +189,6 @@ function changeYear(year){
           return yScale(d[1]);
         })
         .attr("r", 6);
-
-
-
 
 
 
